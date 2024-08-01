81a80b6a3fa44e957b82120c93c6e90195c58fb9
prasadperla55 <137373532+prasadperla55@users.noreply.github.com>
2024-08-01 12:36:49 +0530

    Create redhat-node.sh

---
+++ redhat-node.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Updating system..."
+yum install docker -y 
+systemctl start docker  
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sleep 10
+sudo systemctl enable --now kubelet
